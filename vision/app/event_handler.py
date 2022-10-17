b8727c70701940820f16f2c6ca440714276d1bb8
Shakime Sparks <jeykimers.johnson@gmail.com>
2022-10-17 02:32:37 -0600

    ADD event handler for emotions

---
+++ vision/app/event_handler.py
@@ -0,0 +1,34 @@
+import pika
+import json
+import os
+
+# This function establishes a new connection with the broker
+# and sends the message to a specific queue.
+# INPUT: message : An object containing the analysis of emotions
+def produce(message):
+
+    # set parameter to establish connection
+    host = os.environ['RABBIT_HOST']
+    port = os.environ['RABBIT_PORT']
+    queue1 = os.environ['RABBIT_PRODUCER_QUEUE']
+
+    # create a connection to the locally running rabbitmq message broker
+    # connection_parameters = pika.ConnectionParameters('localhost')
+    connection_parameters = pika.ConnectionParameters(host=host,port=port)
+
+    # save connection by passing our connection parameters
+    connection = pika.BlockingConnection(connection_parameters)
+
+    # store a default channel
+    channel = connection.channel()  # There could be multiples channels.
+    
+    # declare a queue in the channel
+    channel.queue_declare(queue=queue1)
+    
+    # publish message by using a default exchange (empty string)
+    print(message)
+    channel.basic_publish(exchange='', routing_key=queue1, body=json.dumps(message))
+
+    print(f"sent message: {message}")
+
+    connection.close()
