6b3fe4764edc02846ec655a8f065c33f90d8aaa2
Shakime Sparks <jeykimers.johnson@gmail.com>
2022-10-18 00:08:11 -0600

    ADD docker file for backend

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from flask import Flask,render_template, request, jsonify
+from flask import Flask, jsonify
 from flask_mysqldb import MySQL
 import os
 import json
 
 app = Flask(__name__)
 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'emotions_db'
+app.config['MYSQL_HOST'] = os.environ['RABBIT_HOST']
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
 
-# mysql = MySQL(app)
-
+mysql = MySQL(app)
 class Person():
     
     def __init__(self, name, emotion, date):
@@ -26,53 +25,57 @@ class Person():
             "name": self.name,
             "emotion": self.email,
             "date": self.date
-            }
-        
+        }
+
 @app.route('/', methods=['GET'])
 def get_records():
-    # print("get records")
-    # user = User.objects(name='name').first()
-    # if not user:
 
-    #Creating a connection cursor
-    # cursor = mysql.connection.cursor()
+    # Creating a connection cursor
+    cursor = mysql.connection.cursor()
     
-    #Executing SQL Statements
-    # query_string = "SELECT * FROM emotions_analysis"
-    # cursor.execute(query_string)
+    # Executing SQL Statements
+    query_string = "SELECT * FROM emotions_table"
+    cursor.execute(query_string)
     
-    # Saving results
-    # result = cursor.fetchall()
+    # Fetches all rows from the last executed statement
+    result = cursor.fetchall()
+        
+    # output list
+    output = []
     
-    # cursor.close()
-    result = '[{"name": "Arthur", "emotion": "angry", "date": "17/10/2022 07:04 PM"}, {"name": "Cassandra", "emotion": "sad", "date": "17/10/2022 07:04 PM"}, {"name": "Evelyn", "emotion": "undetermined", "date": "17/10/2022 07:04 PM"}]'
-    if not result:
-        return jsonify({'error': 'no data'})
-    return jsonify(result)
+    # Iterate over the rows
+    for row in result:
+        person = Person(row[1],row[2],row[3])
+        output.append(person.to_json())
+        
+    # we close both the cursor and connection
+    cursor.close()
+    
+    return jsonify(output)
     
 def create_record(data):
     
     # Parse string to JSON object
     data = json.loads(data)
     
-    #Creating a connection cursor
-    # cursor = mysql.connection.cursor()
+    # Creating a connection cursor
+    cursor = mysql.connection.cursor()
     
-    # Add information to the table
+    # query to execute
+    query_string = "INSERT INTO emotions_analysis VALUES(%s,%s,%s)"
+    
+    # Iterate and add the information to the table
     print("Adding Data")
     for person in data:
-        print()
-        print(person['name'])
-        print(person['emotion'])
-        print(person['date'])
-
+        
         #Executing SQL Statements
-        # query_string = "INSERT INTO emotions_analysis VALUES(%s,%s,%s)"
-        # cursor.execute(query_string,(name,emotion,date))\
+        cursor.execute(query_string,person['name'],person['emotion'],person['date'])
         
     #Saving the Actions performed on the DB
-    # mysql.connection.commit()
-    # cursor.close()
+    mysql.connection.commit()
+    
+    # we close both the cursor and connection
+    cursor.close()
 
 def run():
     app.run(host='localhost', port=5000)
--- backend/app/event_handler.py
+++ backend/app/event_handler.py
@@ -1,13 +1,13 @@
 # pika: rabbitmq client library to rend or receive messages from our message broker
 import pika
 import os
-import json
 from api import create_record
 
 # This functinon listen to the connection and acts when a message is added to the respective queue.
 # INPUT: body : message
 def on_message_received(ch, method, properties, body):
     print(f'\nreceived: "{body}"')
+    
     # Process the data received
     create_record(body)    
     print(f'\nfinished processing and acknowledged message')
@@ -15,14 +15,13 @@ def on_message_received(ch, method, properties, body):
 # This functinon establish a connection (keeps running until the program is finished) with broker and start a channel to receive message from the queue.
 def consume():
     # set parameter to establish connection
-    # host = os.environ['RABBIT_HOST']
-    # port = os.environ['RABBIT_PORT']
-    # queue = os.environ['RABBIT_CONSUMER_QUEUE']
-    queue = 'emotions_queue'
+    host = os.environ['RABBIT_HOST']
+    port = os.environ['RABBIT_PORT']
+    queue = os.environ['RABBIT_CONSUMER_QUEUE']
     
     # create a connection to the locally running rabbitmq message broker
-    connection_parameters = pika.ConnectionParameters('localhost')
-    # connection_parameters = pika.ConnectionParameters(host=host,port=port)
+    # connection_parameters = pika.ConnectionParameters('localhost')
+    connection_parameters = pika.ConnectionParameters(host=host,port=port)
 
     # save connection by passing our connection parameters
     connection = pika.BlockingConnection(connection_parameters)
